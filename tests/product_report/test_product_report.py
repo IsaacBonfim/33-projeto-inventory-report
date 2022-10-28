57420e6d13ef655c70ebf0a27be7692d7a1bf966
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-10-28 17:51:25 -0300

    Enviando requisito 2

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        'Farinha',
+        'Farinini',
+        '01-05-2021',
+        '02-06-2023',
+        '6354984321',
+        'ao abrigo de luz'
+    )
+
+    assert Product.__repr__(product) == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )

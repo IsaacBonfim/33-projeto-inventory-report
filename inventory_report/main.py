8a2e8f7b7e24b2a0df795c538357d5be6905b490
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-11-05 17:15:34 -0300

    Enviando requisito 11

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,35 @@
+import sys
+from .inventory.inventory_refactor import InventoryRefactor
+from .reports.simple_report import SimpleReport
+from .reports.complete_report import CompleteReport
+from .importer import (csv_importer, json_importer, xml_importer)
+
+
 def main():
-    pass
+    args = sys.argv
+
+    if len(args) < 3:
+        print('Verifique os argumentos', file=sys.stderr)
+        return
+
+    _, path, type = args
+
+    if ".csv" in path:
+        importer = csv_importer.CsvImporter
+    elif ".json" in path:
+        importer = json_importer.JsonImporter
+    else:
+        importer = xml_importer.XmlImporter
+
+    info = InventoryRefactor(importer)
+
+    info.import_data(path, type)
+
+    list = info.data
+
+    if type == "simples":
+        report = SimpleReport
+    else:
+        report = CompleteReport
+
+    print(report.generate(list), end='')
